13bedf75d6554c492b0db32bf81bc6ddc7369ae0
Ethan-Maridat <ethanmrdt@gmail.com>
2024-03-25 09:24:46 +0100

    feat: ajout de la connection

--- lib/UI/ecran1.dart
+++ lib/UI/ecran1.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+import 'shared_pref.dart';
 
-class Ecran1 extends StatelessWidget {
+class Ecran1 extends StatefulWidget {
+  @override
+  _Ecran1State createState() => _Ecran1State();
+}
+
+class _Ecran1State extends State<Ecran1> {
+  String? _loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLoggedInUser();
+  }
+
+  Future<void> _checkLoggedInUser() async {
+    String? loggedInUser = await PreferencesManager.getLoggedInUser();
+    setState(() {
+      _loggedInUser = loggedInUser;
+    });
+  }
+
+  Future<void> _showLoginDialog(BuildContext context) async {
+    TextEditingController pseudoController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Connexion'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: pseudoController,
+                  decoration: InputDecoration(labelText: 'Pseudo'),
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Mot de passe'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: Text('Se connecter'),
+              onPressed: () async {
+                String pseudo = pseudoController.text;
+                String password = passwordController.text;
+                String? savedPassword = await PreferencesManager.getPassword(pseudo);
+
+                if (savedPassword != null && savedPassword == password) {
+                  print('Connexion réussie');
+                  setState(() {
+                    _loggedInUser = pseudo;
+                  });
+                  await PreferencesManager.setLoggedInUser(pseudo);
+                } else {
+                  print('Identifiants incorrects');
+                }
+
+                if (savedPassword == null) {
+                  await PreferencesManager.saveUserCredentials(pseudo, password);
+                  setState(() {
+                    _loggedInUser = pseudo;
+                  });
+                  await PreferencesManager.setLoggedInUser(pseudo);
+                }
+
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue, // Changez la couleur d'arrière-plan selon vos besoins
+      color: Colors.blue,
+      child: Center(
+        child: _loggedInUser != null
+            ? Text('Connecté en tant que $_loggedInUser')
+            : ElevatedButton(
+          onPressed: () {
+            _showLoginDialog(context);
+          },
+          child: Text('Se connecter'),
+        ),
+      ),
     );
   }
 }
---
+++ lib/UI/shared_pref.dart
@@ -0,0 +1,23 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferencesManager {
+  static Future<void> saveUserCredentials(String pseudo, String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(pseudo, password);
+  }
+
+  static Future<String?> getPassword(String pseudo) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(pseudo);
+  }
+
+  static Future<String?> getLoggedInUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('logged_in_user');
+  }
+
+  static Future<void> setLoggedInUser(String pseudo) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('logged_in_user', pseudo);
+  }
+}
