dd98b1ddef81267b61b631311b572335bc24da78
Ethan-Maridat <ethanmrdt@gmail.com>
2024-03-27 10:36:45 +0100

    feat: ajout connexion inscription

--- lib/UI/ecran1.dart
+++ lib/UI/ecran1.dart
@@ -8,6 +8,7 @@ class Ecran1 extends StatefulWidget {
 
 class _Ecran1State extends State<Ecran1> {
   String? _loggedInUser;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>(); // Ajout de la clé globale pour le Scaffold
 
   @override
   void initState() {
@@ -26,6 +27,8 @@ class _Ecran1State extends State<Ecran1> {
     TextEditingController pseudoController = TextEditingController();
     TextEditingController passwordController = TextEditingController();
 
+    String errorMessage = '';
+
     return showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -43,6 +46,8 @@ class _Ecran1State extends State<Ecran1> {
                   obscureText: true,
                   decoration: InputDecoration(labelText: 'Mot de passe'),
                 ),
+                if(errorMessage.isNotEmpty)
+                  Text(errorMessage, style: TextStyle(color: Colors.red)),
               ],
             ),
           ),
@@ -60,15 +65,29 @@ class _Ecran1State extends State<Ecran1> {
                 String password = passwordController.text;
                 String? savedPassword = await PreferencesManager.getPassword(pseudo);
 
-                if (savedPassword != null && savedPassword == password) {
+                if (savedPassword == null) {
+                  errorMessage = 'Pseudo inexistant';
+                  _showErrorMessage(errorMessage);
+                } else if (savedPassword != password) {
+                  errorMessage = 'Mot de passe incorrect';
+                  _showErrorMessage(errorMessage);
+                } else {
                   print('Connexion réussie');
                   setState(() {
                     _loggedInUser = pseudo;
                   });
                   await PreferencesManager.setLoggedInUser(pseudo);
-                } else {
-                  print('Identifiants incorrects');
+                  Navigator.of(context).pop();
+                  _showSuccessMessage('Connexion réussie');
                 }
+              },
+            ),
+            ElevatedButton(
+              child: Text('S\'inscrire'),
+              onPressed: () async {
+                String pseudo = pseudoController.text;
+                String password = passwordController.text;
+                String? savedPassword = await PreferencesManager.getPassword(pseudo);
 
                 if (savedPassword == null) {
                   await PreferencesManager.saveUserCredentials(pseudo, password);
@@ -76,9 +95,12 @@ class _Ecran1State extends State<Ecran1> {
                     _loggedInUser = pseudo;
                   });
                   await PreferencesManager.setLoggedInUser(pseudo);
+                  Navigator.of(context).pop();
+                  _showSuccessMessage('Inscription réussie');
+                } else {
+                  errorMessage = 'Le pseudo existe déjà';
+                  _showErrorMessage(errorMessage);
                 }
-
-                Navigator.of(context).pop();
               },
             ),
           ],
@@ -94,31 +116,66 @@ class _Ecran1State extends State<Ecran1> {
     });
   }
 
+  // Fonction pour afficher le message d'erreur dans une boîte de dialogue
+  void _showErrorMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Erreur"),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Fonction pour afficher le message de succès avec SnackBar
+  void _showSuccessMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message),
+      duration: Duration(seconds: 2), // Durée d'affichage du message
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Ecran 1'),
-        leading: _loggedInUser != null
-            ? IconButton(
-          icon: Icon(Icons.logout),
-          onPressed: () {
-            _logout();
-          },
-        )
-            : null,
-      ),
+      key: _scaffoldKey, // Ajout de la clé globale au Scaffold
       body: Container(
         color: Colors.blue,
-        child: Center(
-          child: _loggedInUser != null
-              ? Text('Connecté en tant que $_loggedInUser')
-              : ElevatedButton(
-            onPressed: () {
-              _showLoginDialog(context);
-            },
-            child: Text('Se connecter'),
-          ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 40,
+              left: 10,
+              child: _loggedInUser != null
+                  ? IconButton(
+                icon: Icon(Icons.logout),
+                onPressed: () {
+                  _logout();
+                },
+              )
+                  : Container(),
+            ),
+            Center(
+              child: _loggedInUser != null
+                  ? Text('Connecté en tant que $_loggedInUser')
+                  : ElevatedButton(
+                onPressed: () {
+                  _showLoginDialog(context);
+                },
+                child: Text('Se connecter'),
+              ),
+            ),
+          ],
         ),
       ),
     );
